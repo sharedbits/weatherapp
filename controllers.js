2311f6c1e6f3fb23c49a67e9275f3e69b9c4c928
YannLelong <lelong.yann@gmail.com>
2015-05-03 16:01:21 +0200

    Revert "Revert "New functionalities""
    This reverts commit ae2e1e66183988a47fff7bdbec753f07ba42a33e.

---
+++ controllers.js
@@ -0,0 +1,15 @@
+var app = angular.module('myApp', []);
+app.controller('myController', function($scope, $http) {
+	$scope.slctdCity = false;
+	$scope.error = false;
+	$scope.cityName = "Lyon";
+	$scope.searchName = function(){//will display a list of the different cities found, else the weather info about the only city found, else error message
+		searchByName($scope, $http);	
+	}
+	$scope.searchCoord = function(){//will display the weather info about the city nearest to the entered coordinates
+		searchByCoord($scope, $http);
+	}
+	$scope.citySlct = function(id){//will display the weather info about the city clicked in the proposed list
+		citySelection($scope, $http, id);
+	}
+});
